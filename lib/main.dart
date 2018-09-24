5ad83aa4d8183f1d1ee540ab1dafbd35e0c9b779
ulfah cn <ulfahcn@gmail.com>
2018-09-24 15:41:17 +0700

    Bab ListView Widget (1)

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
-import 'model_user.dart';
-import 'helloworld.dart';
+import 'page_list.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-      print(users[0].first+" "+users[0].last);
       return MaterialApp(
         title: "Aplikasi Pertama",
-        home: new HelloWorld(),
+        home: new PageList(),
       );
   }
 }
---
+++ lib/page_list.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PageList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+      return Text("Sementara");
+    }
+}
