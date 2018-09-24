8083c2b5137df7472e9ab1ed14a4c569fef1a859
ulfah cn <ulfahcn@gmail.com>
2018-09-24 21:55:29 +0700

    Bab ListView Widget (2)

--- lib/page_list.dart
+++ lib/page_list.dart
@@ -3,6 +3,24 @@ import 'package:flutter/material.dart';
 class PageList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-      return Text("Sementara");
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("List User"),
+        ),
+        body: new ListView(
+          children: <Widget>[
+            ListTile(
+              title: new Text("Nama"),
+              subtitle: new Text("alamat_email@domain.com"),
+              leading: new CircleAvatar(child: new Text("N"),),
+            ),
+            ListTile(
+              title: new Text("Nama"),
+              subtitle: new Text("alamat_email@domain.com"),
+              leading: new CircleAvatar(child: new Text("N"),),
+            ),
+          ],
+        ),
+      );
     }
 }
