548bdda818a48746fd7b253abc21e510d3babe46
ulfah cn <ulfahcn@gmail.com>
2018-10-10 16:18:51 +0700

    Bab Basic Layouting & Stateful Widget

--- lib/helloworld.dart
+++ lib/helloworld.dart
@@ -9,14 +9,131 @@ class HelloWorld extends StatelessWidget {
           appBar: new AppBar(
             title: new Text("Hello World"),
           ),
-          body: new Center(
-            child: RaisedButton(
-              child: Text('Lihat List User'),
-              onPressed: () {
-                Navigator.pushNamed(context, '/pagelist');
-              },
-            ),
-          ),
+          body: MenuutamaWidget(),
+          backgroundColor: Colors.grey[400],
       );
     }
+}
+
+class MenuutamaWidget extends StatefulWidget {
+  @override
+  MenuutamaWidgetState createState()=> new MenuutamaWidgetState();
+}
+
+class MenuutamaWidgetState extends State<MenuutamaWidget> {
+
+  int _visitingCounter = 0;
+
+  void pagelistVisited() {
+    setState(() {
+      _visitingCounter = _visitingCounter + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey[500]),
+            color: Colors.white,
+          ),
+          height: 110.0,
+          margin: EdgeInsets.all(8.0),
+          child: Row(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(right:2.0),
+                child: Image.asset('images/sample1.jpg'),
+              ),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text("List User", style: TextStyle(
+                        fontSize:18.0,
+                        color: Colors.grey[700],
+                      )),
+                      Text("Menampilkan data user.", style: TextStyle(
+                        color: Colors.grey[500],
+                      )),
+                      Spacer(),
+                      Row(children: <Widget>[
+                        Spacer(),
+                        Icon(Icons.remove_red_eye),
+                        Text(_visitingCounter.toString()),
+                        Spacer(),
+                        Icon(Icons.share),
+                        Spacer(),
+                        RaisedButton(
+                          child: Text("Detail"),
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/pagelist');
+                            pagelistVisited();
+                          },
+                        ),
+                        Spacer(),
+                      ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey[500]),
+            color: Colors.white,
+          ),
+          height: 110.0,
+          margin: EdgeInsets.all(8.0),
+          child: Row(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(right:2.0),
+                child: Image.asset('images/sample2.jpg'),
+              ),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text("List Admin", style: TextStyle(
+                        fontSize:18.0,
+                        color: Colors.grey[700],
+                      )),
+                      Text("Menampilkan administrator.", style: TextStyle(
+                        color: Colors.grey[500],
+                      )),
+                      Spacer(),
+                      Row(children: <Widget>[
+                        Spacer(),
+                        Icon(Icons.remove_red_eye),
+                        Text("12"),
+                        Spacer(),
+                        Icon(Icons.share),
+                        Spacer(),
+                        RaisedButton(
+                          child: Text("Detail"),
+                          onPressed: () {},
+                        ),
+                        Spacer(),
+                      ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
 }
